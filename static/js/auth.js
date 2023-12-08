ba3825ea6349107c4896290df42c102600c6a464
Muhammed Ramadan <mhdrdn122@gmail.com>
2023-12-08 04:38:26 -0800

    add auth.js

---
+++ static/js/auth.js
@@ -0,0 +1,6 @@
+let user_name = document.getElementById("user-name")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let sign_up = document.getElementById("sign_up")
+
+console.log()
